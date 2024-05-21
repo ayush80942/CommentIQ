265a1fde77a0d99a85dd3ce2343ff94f49ce001e
Ayush Aggarwal <ayushaggarwal@Ayushs-Air.bbrouter>
2024-05-22 01:31:18 +0530

    Pushed the updated api

--- app.py
+++ app.py
@@ -2,13 +2,26 @@ from flask import Flask, render_template, url_for, request
 
 app = Flask(__name__)
 
+def extract_video_id(youtube_link):
+    index = youtube_link.index('=')
+    video_id = youtube_link[index + 1:]
+    return video_id
+
 @app.route('/')
 def welcome():
     return render_template("index.html")
 
 @app.route('/dashboard')
 def dashboard():
-    url = request.form.values
+    return render_template("dashboard.html")
+
+@app.route('/submit', methods = ['GET', 'POST'])
+def submit():
+    url = ''
+    if request.method == 'POST':
+        url = request.form['url']
+    videoID = extract_video_id(url)
+    print(videoID)
     return render_template("dashboard.html")
 
 if __name__ == '__main__':
