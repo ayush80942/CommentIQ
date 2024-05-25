960dfe3a667a6555fc89da920171c8c1d1bc2833
Ayush Aggarwal <ayushaggarwal@Ayushs-MacBook-Air.local>
2024-05-25 23:36:01 +0530

    API & UI  Changes

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
 from flask import Flask, render_template, url_for, request
+import googleapiclient.discovery
+import pandas as pd
+
+api_service_name = "youtube"
+api_version = "v3"
+DEVELOPER_KEY = "AIzaSyD4ewMWP_BoxjLTG_zgMbRlycPvYs7vaZs"
 
 app = Flask(__name__)
 
 def extract_video_id(youtube_link):
-    index = youtube_link.index('=')
-    video_id = youtube_link[index + 1:]
-    return video_id
+    if "v=" in youtube_link:
+        return youtube_link.split("v=")[-1].split("&")[0]
+    elif "youtu.be/" in youtube_link:
+        return youtube_link.split("youtu.be/")[-1].split("?")[0]
+    else:
+        raise ValueError("Invalid YouTube URL")
 
 @app.route('/')
 def welcome():
@@ -22,6 +31,33 @@ def submit():
         url = request.form['url']
     videoID = extract_video_id(url)
     print(videoID)
+
+    youtube = googleapiclient.discovery.build(api_service_name, api_version, developerKey=DEVELOPER_KEY)
+
+    yt_request = youtube.commentThreads().list(
+        part="snippet",
+        videoId = videoID,
+        maxResults=100
+    )
+    response = yt_request.execute()
+
+    comments = []
+
+    for item in response['items']:
+        comment = item['snippet']['topLevelComment']['snippet']
+        comments.append([
+            comment['authorDisplayName'],
+            comment['publishedAt'],
+            comment['updatedAt'],
+            comment['likeCount'],
+            comment['textDisplay']
+        ])
+
+    df = pd.DataFrame(comments, columns=['author', 'published_at', 'updated_at', 'like_count', 'text'])
+    df = df.drop(['author','published_at','updated_at','like_count'],axis=1)
+    df.rename(columns = {'text':'Comment'}, inplace = True)
+    print(df.head())
+
     return render_template("dashboard.html")
 
 if __name__ == '__main__':
